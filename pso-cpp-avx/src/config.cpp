98567e48320fb0b7db8fa4392f58affeac1f607a
r4qq <r4qq@protonmail.com>
2025-04-07 13:15:58 +0200

    TODO: more testing; config.h/conig.c done

--- pso-cpp-avx/src/config.cpp
+++ pso-cpp-avx/src/config.cpp
@@ -1,47 +1,45 @@
 #include <immintrin.h>
 #include "config.h"
 
-void avx512VectorAdd(const std::vector<double>& vA, const std::vector<double>& vB, std::vector<double>& vResult) {
+#ifdef __AVX512F__
+void avx512VectorAdd(const std::vector<double>& vA, const std::vector<double>& vB, std::vector<double>& vResult) 
+{
     size_t vecSize = vA.size();
     size_t remainderElementsAmount = 0;
-
-    for (size_t i = 0; i <= vecSize - 8; i += 8)               //double is 64bit so 512 / 64 is 8
+    for (size_t i = 0; i <= vecSize - 8; i += 8) 
     {
-        __m512d nVA = _mm512_loadu_pd(vA.data() + i);       //load vectors to registers
+        __m512d nVA = _mm512_loadu_pd(vA.data() + i);
         __m512d nVB = _mm512_loadu_pd(vB.data() + i);
-        __m512d nVR = _mm512_add_pd(nVA, nVB);           //add from registers
-        
-        _mm512_storeu_pd(vResult.data() + i, nVR);       //save result to result vector
-        
+        __m512d nVR = _mm512_add_pd(nVA, nVB);
+        _mm512_storeu_pd(vResult.data() + i, nVR);
         remainderElementsAmount += 8;
     }
-
     for (auto i = remainderElementsAmount; i < vecSize; ++i) 
     {
         vResult[i] = vA[i] + vB[i];
     }
 }
+#endif
 
-void avx2VectorAdd(const std::vector<double>& vA, const std::vector<double>& vB, std::vector<double>& vResult) {
+#ifdef __AVX2__
+void avx2VectorAdd(const std::vector<double>& vA, const std::vector<double>& vB, std::vector<double>& vResult) 
+{
     size_t vecSize = vA.size();
     size_t remainderElementsAmount = 0;
-
-    for (size_t i = 0; i <= vecSize - 4; i += 4)               //double is 64bit so 256 / 64 is 4
+    for (size_t i = 0; i <= vecSize - 4; i += 4) 
     {
-        __m256d nVA = _mm256_loadu_pd(vA.data() + i);       //load vectors to registers
+        __m256d nVA = _mm256_loadu_pd(vA.data() + i);
         __m256d nVB = _mm256_loadu_pd(vB.data() + i);
-        __m256d nVR = _mm256_add_pd(nVA, nVB);           //add from registers
-        
-        _mm256_storeu_pd(vResult.data() + i, nVR);       //save result to result vector
-        
+        __m256d nVR = _mm256_add_pd(nVA, nVB);
+        _mm256_storeu_pd(vResult.data() + i, nVR);
         remainderElementsAmount += 4;
     }
-
     for (auto i = remainderElementsAmount; i < vecSize; ++i) 
     {
         vResult[i] = vA[i] + vB[i];
     }
 }
+#endif
 
 void scalarVectorAdd(const std::vector<double>& vA, const std::vector<double>& vB, std::vector<double>& vResult) 
 {
--- pso-cpp-avx/src/config.h
+++ pso-cpp-avx/src/config.h
@@ -5,31 +5,33 @@
 
 #include <vector>
 #include <stdexcept>
+#include <iostream>
 
+#ifdef __AVX512F__
 void avx512VectorAdd(const std::vector<double>& vA, const std::vector<double>& vB, std::vector<double>& vResult);
+#endif
+#ifdef __AVX2__
 void avx2VectorAdd(const std::vector<double>& vA, const std::vector<double>& vB, std::vector<double>& vResult);
+#endif
 void scalarVectorAdd(const std::vector<double>& vA, const std::vector<double>& vB, std::vector<double>& vResult);
 
 inline void avxVectorAdd(const std::vector<double>& vA, const std::vector<double>& vB, std::vector<double>& vResult)
 {
-    if(vA.size() != vB.size() || vA.size() != vResult.size() || vB.size() != vResult.size())
+    if (vA.size() != vB.size() || vA.size() != vResult.size())
     {
         throw std::runtime_error("Vector size mismatch");
     }
 
-    if(__builtin_cpu_supports("avx512f"))
-    {
-        avx512VectorAdd(vA, vB, vResult);
-    }
-    else if (__builtin_cpu_supports("avx2"))
-    {
-        avx2VectorAdd(vA, vB, vResult);
-    }
-    else 
-    {
-        scalarVectorAdd(vA, vB, vResult);
-    }
+#ifdef __AVX512F__
+    avx512VectorAdd(vA, vB, vResult);
+    std::cout << "AVX512 detected" << std::endl;
+#elif defined(__AVX2__)
+    avx2VectorAdd(vA, vB, vResult);
+    std::cout << "AVX2 detected" << std::endl;
+#else
+    scalarVectorAdd(vA, vB, vResult);
+    std::cout << "No avx detected, using scalar" << std::endl;
+#endif
 }
 
-
-#endif 
+#endif
