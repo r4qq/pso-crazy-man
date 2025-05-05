c0bd6d3d252fee72396c75ab5c374c7f1bfd0da5
r4qq <r4qq@protonmail.com>
2025-05-05 12:07:58 +0200

    i forgor

--- pso-rust/src/main.rs
+++ pso-rust/src/main.rs
@@ -4,20 +4,12 @@ mod point;
 use point::Point;
 fn main() 
 {
-    
-    
+    let EPOCH = 100;
+    let POINTSAMOUNT = 2000;
+    let BOUND: (f64, f64) = (-10.0, 10.0);
+    let ALPHA = 0.5;
+    let BETA = 0.3; 
+    let POINTDIMENSION = 8; //set to 2 for func1, to 4 for func2, 8 fo func3
+    let SAME_GRADE_EPOCHS = 10;
 
-    println!("Hello, world!");
-
-    let x =  5;
-
-    let mut y = 10;
-
-    y += x;
-
-    println!("y is {}", y);
-
-    let s1 = String::from("Hello");
-    let s2 = s1;
-    println!("{}", s2);
 }
--- pso-rust/src/point.rs
+++ pso-rust/src/point.rs
@@ -1,6 +1,6 @@
 pub struct Point 
 {
-    positon: Vec<f64>,
+    position: Vec<f64>,
     velocityVector: Vec<f64>,
     personalBest: Vec<f64>,
     grade: f64,
@@ -12,7 +12,7 @@ impl Point
     pub fn new(startPos: Vec<f64>, startVelocity: Vec<f64>) -> Self 
     {
         Point {
-            positon: startPos.clone(),
+            position: startPos.clone(),
             velocityVector: startVelocity,
             personalBest: startPos.clone(),
             grade: f64::MAX,
@@ -20,8 +20,28 @@ impl Point
         }
     }    
 
-    pub fn updatePosition(&mut self) 
+    pub fn updatePosition(&mut self)
     {
-        
+        for i in 0..self.position.len()
+        {
+            self.position[i] += self.velocityVector[i];
+        }
+    }
+
+    pub fn updateVelocity(&mut self, alpha: f64, beta: f64, epsilon1: f64, epsilon2: f64, globalBest: &Vec<f64>)
+    {
+        for i in 0..self.velocityVector.len()
+        {
+            self.velocityVector[i] += (alpha * epsilon1 * (globalBest[i] - self.position[i]) + 
+                                       beta * epsilon2 * (self.personalBest[i] - self.position[i]));
+        }
+    }
+
+    pub fn enforceBounds(&mut self, bounds: (f64, f64))
+    {
+        for i in 0..self.velocityVector.len()
+        {
+            self.position[i] = bounds.0.max(bounds.1.min(self.position[i]));
+        }
     }
 }
