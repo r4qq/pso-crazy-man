ae949705c7abb3a3474abb24ae9296a5bf9422c3
r4qq <r4qq@protonmail.com>
2025-10-27 09:01:16 +0100

    add intertia and update naming convention for pythin

--- pso-python/pso.py
+++ pso-python/pso.py
@@ -1,3 +1,4 @@
+import numpy as np
 import random
 import math
 import matplotlib.pyplot as plt
@@ -6,99 +7,103 @@ import time
 from typing import List, Callable
 
 EPOCH = 50
-POINTSAMOUNT = 200000
+POINTS_AMOUNT = 200000
 BOUND = [-10.0, 10.0]
 ALPHA = 0.5
 BETA = 0.3
-POINTDEMENSION = 8
+POINT_DIMENSION = 8
+INERTIA = 0.7
+SAME_GRADE_EPOCHS = 10
 
 class Point:
-    def __init__(self, position: List[float], velocityVector: List[float]) -> None:
+    def __init__(self, position: List[float], velocity_vector: List[float]) -> None:
         self.position = position
-        self.velocityVector = velocityVector
-        self.personalBest = position
+        self.velocity_vector = velocity_vector
+        self.personal_best = position
         self.grade = float('inf')
 
-    def updateVelocity(self, alpha: float, beta: float, globalBest: List[float]) -> None:
+    def update_velocity(self, alpha: float, beta: float, globalBest: List[float], inertia: float) -> None:
         epsilon1, epsilon2 = random.random(), random.random()
-        for i in range(len(self.velocityVector)):
-            self.velocityVector[i] += (alpha * epsilon1 * (globalBest[i] - self.position[i]) +
-                                        beta * epsilon2 * (self.personalBest[i] - self.position[i]))
+        for i in range(len(self.velocity_vector)):
+            self.velocity_vector[i] = self.velocity_vector[i] * inertia + (alpha * epsilon1 * (globalBest[i] - self.position[i]) +
+                                        beta * epsilon2 * (self.personal_best[i] - self.position[i]))
 
-    def updatePosition(self) -> None:
+    def update_position(self) -> None:
         for i in range(len(self.position)):
-            self.position[i] += self.velocityVector[i]
+            self.position[i] += self.velocity_vector[i]
 
-    def evalPoint(self, func: Callable) -> None:
-        currentGrade = func(self.position)
-        if currentGrade < self.grade:  
-            self.personalBest = self.position
-            self.grade = currentGrade  
+    def eval_point(self, func: Callable) -> None:
+        current_grade = func(self.position)
+        if current_grade < self.grade:  
+            self.personal_best = self.position
+            self.grade = current_grade  
 
 class Algo:
-    def __init__(self, epoch: int, pointsAmount: int, bound: List[float],
-                       alpha: float, beta: float, funcToMinimize: Callable, sameGradepochs: int) -> None:
+    def __init__(self, epoch: int, points_Amount: int, bound: List[float],
+                    alpha: float, beta: float, func_to_minimize: Callable, 
+                    same_grade_epochs: int, inertia: float) -> None:
         self.epoch = epoch
-        self.pointsAmount = pointsAmount
+        self.points_Amount = points_Amount
         self.bound = bound
         self.alpha = alpha
         self.beta = beta
-        self.funcToMinimize = funcToMinimize
-        self.sameGradepochs = sameGradepochs
+        self.func_to_minimize = func_to_minimize
+        self.same_grade_epochs = same_grade_epochs
         self.points = self._initPoints()
-        self.globalBestPos = None
-        self.globalBestVal = float('inf')
-        self.bestGradesHistory = []
-        self.consecutiveUnchangedEpochs = 0
+        self.global_best_pos = None
+        self.global_best_val = float('inf')
+        self.best_grades_history = []
+        self.consecutive_unchanged_epochs = 0
+        self.inertia = inertia
 
     def _initPoints(self) -> None:
         points = []
-        for _ in range(self.pointsAmount):
-            startPos = [random.uniform(self.bound[0], self.bound[1]) for _ in range(POINTDEMENSION)]
-            velocityVector = [random.uniform(-1, 1) for _ in range(POINTDEMENSION)]
-            point = Point(startPos, velocityVector)
-            point.grade = self.funcToMinimize(startPos)
+        for _ in range(self.points_Amount):
+            startPos = [random.uniform(self.bound[0], self.bound[1]) for _ in range(POINT_DIMENSION)]
+            velocity_vector = [random.uniform(-1, 1) for _ in range(POINT_DIMENSION)]
+            point = Point(startPos, velocity_vector)
+            point.grade = self.func_to_minimize(startPos)
             points.append(point)
         return points
 
     def _updateGlobalBest(self) -> bool:
         tempPoint = min(self.points, key=lambda p: p.grade)
-        if tempPoint.grade < self.globalBestVal:
-            self.globalBestVal = tempPoint.grade
-            self.globalBestPos = tempPoint.position
+        if tempPoint.grade < self.global_best_val:
+            self.global_best_val = tempPoint.grade
+            self.global_best_pos = tempPoint.position
             return True
         return False
 
     def optimize(self) -> tuple:
-        startTime = time.time()
+        start_time = time.time()
 
         for eepoch in range(self.epoch):
             optimized = self._updateGlobalBest()
-            self.bestGradesHistory.append(self.globalBestVal)
+            self.best_grades_history.append(self.global_best_val)
             for point in self.points:
-                point.updateVelocity(self.alpha, self.beta, self.globalBestPos)
-                point.updatePosition()
-                point.evalPoint(self.funcToMinimize)
+                point.update_velocity(self.alpha, self.beta, self.global_best_pos, self.inertia)
+                point.update_position()
+                point.eval_point(self.func_to_minimize)
 
             if optimized:
-                self.consecutiveUnchangedEpochs = 0
+                self.consecutive_unchanged_epochs = 0
             else:
-                self.consecutiveUnchangedEpochs += 1
-                if self.consecutiveUnchangedEpochs >= self.sameGradepochs:
+                self.consecutive_unchanged_epochs += 1
+                if self.consecutive_unchanged_epochs >= self.same_grade_epochs:
                     break
         
-        endTime = time.time()
-        print(f"Optimization completed in {endTime - startTime:.4f} seconds")
+        end_time = time.time()
+        print(f"Optimization completed in {end_time - start_time:.4f} seconds")
         print(f"in {eepoch} epochs")
-        return self.globalBestPos, self.globalBestVal
+        return self.global_best_pos, self.global_best_val
 
 if __name__ == "__main__":
 
-    def funcToMinimize(vars):
+    def func_to_minimize(vars):
         x, y = vars
         return math.sin(x**4) - math.cos(y**2) + 6 * (x**2) * (y**2)
 
-    def funcToMinimize2(vars):
+    def func_to_minimize2(vars):
         x1, x2, x3, x4, x5, x6, x7, x8 = vars
     
         return (math.sin(math.pow(x1, 4)) + 
@@ -109,14 +114,14 @@ if __name__ == "__main__":
                3 * math.pow(x7, 2) * math.pow(x8, 2) +  
                2 * math.pow(x1, 2) + math.pow(x2, 2) +  math.pow(x3, 2) +  math.pow(x4, 2)) 
 
-    pso = Algo(EPOCH, POINTSAMOUNT, BOUND, ALPHA, BETA, funcToMinimize2, 15)
-    bestPos, bestVal = pso.optimize()
+    pso = Algo(EPOCH, POINTS_AMOUNT, BOUND, ALPHA, BETA, func_to_minimize2, SAME_GRADE_EPOCHS, INERTIA)
+    best_pos, best_val = pso.optimize()
 
     print("-------------")
-    print(bestPos)
-    print(bestVal)
+    print(best_pos)
+    print(best_val)
 
-    plt.plot(pso.bestGradesHistory)
+    plt.plot(pso.best_grades_history)
     plt.xlabel("Epochs")
     plt.ylabel("Grades")
     plt.title("Best Grades Over Epochs")
